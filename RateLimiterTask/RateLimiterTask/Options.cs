27609bb21dc916b3e7d9f2c20a5285ff37541722
ap-Camel <101003521+ap-Camel@users.noreply.github.com>
2022-11-04 07:22:30 +0100

    added project in my own GH repo

---
+++ RateLimiterTask/RateLimiterTask/Options.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RateLimiterTask
+{
+
+    // class for mapping configuration options from appsettings.json
+    public class Options
+    {
+
+
+        public class EndpointLimit
+        {
+            public string Endpoint { get; set; }
+            public int RequestLimitMs { get; set; }
+            public int RequestLimitCount { get; set; }
+        }
+
+
+        public bool RequestLimiterEnabled { get; set; } = true;
+        public int DefaultRequestLimitMs { get; set; } = 1000;
+        public int DefaultRequestLimitCount { get; set; } = 5;
+        public List<EndpointLimit> EndpointLimits { get; set; }
+
+    }
+}
